94130d02d4a9c8435cb6587f49f879bbeebde7d9
Onur Altuntas <onuraltuntas50@gmail.com>
2019-08-01 20:29:54 +0300

    Splash Animation is done.

--- app/src/main/java/com/example/ortalamahesapla/Splash_activity.kt
+++ app/src/main/java/com/example/ortalamahesapla/Splash_activity.kt
@@ -3,6 +3,7 @@ package com.example.ortalamahesapla
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class Splash_activity : AppCompatActivity() {
@@ -16,6 +17,12 @@ class Splash_activity : AppCompatActivity() {
             startActivity(intent)
 
         }
+
+        var asagidanGelenButon=AnimationUtils.loadAnimation(this,R.anim.asagidanbuton)
+        btnHesaplaSplash.animation=asagidanGelenButon
+
+        var yukaridanGelenBalon=AnimationUtils.loadAnimation(this,R.anim.yukaridanbalon)
+        imgBalon.animation=yukaridanGelenBalon
     }
 
 }
